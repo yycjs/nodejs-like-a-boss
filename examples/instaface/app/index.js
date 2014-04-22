8fb59fcb5279f9a5b40d5b8532a35e8b8650f493
David Luecke <daff@neyeon.com>
2014-04-22 16:00:13 -0600

    Adding login.

--- examples/instaface/app/index.js
+++ examples/instaface/app/index.js
@@ -2,8 +2,16 @@ var feathers = require('feathers');
 var memory = require('feathers-memory');
 var path = require('path');
  
-feathers().configure(feathers.socketio())
+feathers().configure(feathers.socketio(function(io) {
+    var app = this;
+
+    io.set('authorization', function (handshake, callback) {
+      app.lookup('users').create({
+        username: handshake.query.username
+      }, {}, callback);
+    });
+  }))
   .use(feathers.static(path.join(__dirname, '..', 'public')))
-  .use('/users', memory)
-  .use('/images', memory)
+  .use('/users', memory())
+  .use('/images', memory())
   .listen(1337);
--- examples/instaface/public/js/main.js
+++ examples/instaface/public/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+  var socket;
+
+  $('button.login').on('click', function() {
+    socket = io.connect('http://localhost:1337', {
+      query: $.param({ username: $('#username').val() })
+    });
+
+    socket.on('images created', function(image) {
+      // '<div class="col-3 left"><img src="" alt=""><h3 class="center-text">Eric</h3></div>'
+    });
+
+    $('.user-modal').addClass('hidden');
+  });
+
 
   $('a.close').click(function(ev){
     ev.preventDefault();
